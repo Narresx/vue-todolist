d7155d9d6eb36cf2d2ac80746a59ff292d3c6913
Narresx <92748159+Narresx@users.noreply.github.com>
2022-01-28 17:06:40 +0100

    Fix link css and add delate class

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ const root = new Vue({
 
     data: {
         tasks: [
-            { text: 'Capire se ho capito', done: false, },
+            { text: 'Capire se ho capito', done: true, },
             { text: 'Controllare la propria sanità mentale', done: false, },
             { text: 'Usare meno il meme di pepe e i suoi derivati', done: false, },
             { text: 'Trattare meglio la paperella', done: false, },
@@ -19,6 +19,7 @@ const root = new Vue({
     },
 
     methods: {
+
         removeTask(index) {
             this.tasks.splice(index, 1); // splice è un metodo che permette di eliminare elementi che ammette 2 parametri, il primo è il punto di partenza, il secondo deve indicare quanti elementi deve cancellare dal punto di partenza
         },
