faa0e63e6494fce80c92fec678b88909fadb25ca
Narresx <92748159+Narresx@users.noreply.github.com>
2022-01-28 16:20:03 +0100

    add splice at x button

--- script.js
+++ script.js
@@ -16,5 +16,11 @@ const root = new Vue({
             { text: 'Trattare meglio la paperella', done: false, },
             { text: 'Evitare attacchi di panico', done: false, },
         ],
-    }
+    },
+
+    methods: {
+        removeTask(index) {
+            this.tasks.splice(index, 1); // splice è un metodo che permette di eliminare elementi che ammette 2 parametri, il primo è il punto di partenza, il secondo deve indicare quanti elementi deve cancellare dal punto di partenza
+        },
+    },
 });
